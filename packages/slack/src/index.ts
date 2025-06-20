3b271bcc59c1b7b53104b8f38764cc23009d82f0
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-06-21 03:48:34 +0530

    chore: update @microfox/slack package with new configuration files and dependencies

--- packages/slack/src/index.ts
+++ packages/slack/src/index.ts
@@ -1,9 +1,4 @@
-export {
-  WebClient,
-  WebClientOptions,
-  WebAPICallResult,
-  WebClientEvent,
-  ErrorCode as SlackErrorCode
-} from '@slack/web-api';
+export type { WebClientOptions, WebAPICallResult } from '@slack/web-api';
+export type { WebClient, WebClientEvent, ErrorCode as SlackErrorCode } from '@slack/web-api';
 
-export { MicrofoxSlackClient } from './MicrofoxSlackClient'; 
+export { MicrofoxSlackClient } from './MicrofoxSlackClient';
