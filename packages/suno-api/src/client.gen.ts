610023bd1524fdf2a27ad657f6afe996d15fdf37
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-07-06 01:35:16 +0530

    fix: update base URL in client configuration to use environment variable

--- packages/suno-api/src/client.gen.ts
+++ packages/suno-api/src/client.gen.ts
@@ -14,5 +14,5 @@ import { type Config, type ClientOptions as DefaultClientOptions, createClient, 
 export type CreateClientConfig<T extends DefaultClientOptions = ClientOptions> = (override?: Config<DefaultClientOptions & T>) => Config<Required<DefaultClientOptions> & T>;
 
 export const client = createClient(createConfig<ClientOptions>({
-    baseUrl: 'https://microfox-suno-api.vercel.app'
+    baseUrl: process.env.SUNO_API_BASE_URL
 }));
