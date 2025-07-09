28c639191961de34e1359326b71ac0615784d52d
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-07-09 17:51:13 +0530

    feat: export generateCode.v2 for enhanced code generation

--- packages/ai-agent/src/index.ts
+++ packages/ai-agent/src/index.ts
@@ -4,4 +4,5 @@ export * from './schemas';
 export * from './utils/scanDirectoryStructure';
 export * from './generateCodeFile';
 export * from './generateCode.v0';
+export * from './generateCode.v2';
 export * from './generateProject';
