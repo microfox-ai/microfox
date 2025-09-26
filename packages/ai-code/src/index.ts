855840b4d743ddc665fef8a1e12d807916f9e6f9
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-09-26 12:50:20 +0530

    chore: remove deprecated AI provider packages and related documentation

--- packages/ai-code/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Main exports for ai-code
-export * from './types';
-export * from './schemas';
-export * from './utils/scanDirectoryStructure';
-export * from './generateCodeFile';
-export * from './generateCode.v0';
-export * from './generateCode.v2';
-export * from './generateProject';
--- packages/ai-provider-vertex/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './vertexProvider';
-export * from './types'; 
